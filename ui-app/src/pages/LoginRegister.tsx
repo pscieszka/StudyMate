4231a785c199cfb8110d74e9ea47b2a4f21cee03
Emilia <emilkatrybala@gmail.com>
2025-01-19 20:05:10 +0100

    obsluga bledow przy logowaniu i rejestracji

--- ui-app/src/pages/LoginRegister.tsx
+++ ui-app/src/pages/LoginRegister.tsx
@@ -1,146 +1,178 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React, {useState} from "react";
+import {useNavigate} from "react-router-dom";
 import "./LoginRegister.css";
 
 interface LoginRegisterProps {
-  setIsAuthenticated: (value: boolean) => void;
+    setIsAuthenticated: (value: boolean) => void;
 }
 
-const LoginRegister: React.FC<LoginRegisterProps> = ({ setIsAuthenticated }) => {
-  const [isLogin, setIsLogin] = useState(true);
-  const [formData, setFormData] = useState({
-    username: "",
-    email: "",
-    password: "",
-  });
-  const [error, setError] = useState("");
-  const navigate = useNavigate();
+const LoginRegister: React.FC<LoginRegisterProps> = ({setIsAuthenticated}) => {
+    const [isLogin, setIsLogin] = useState(true);
+    const [formData, setFormData] = useState({
+        username: "",
+        email: "",
+        password: "",
+    });
+    const [error, setError] = useState("");
+    const navigate = useNavigate();
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData({...formData, [e.target.name]: e.target.value});
+    };
 
-  const fetchUserInfo = async () => {
-    try {
-      const token = sessionStorage.getItem("accessToken");
-      if (!token) {
-        throw new Error("Token is missing. Please log in again.");
-      }
+    const fetchUserInfo = async () => {
+        try {
+            const token = sessionStorage.getItem("accessToken");
+            if (!token) {
+                throw new Error("Token is missing. Please log in again.");
+            }
 
-      const response = await fetch("http://localhost:8000/api/userinfo", {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${token}`,
-          "Content-Type": "application/json",
-        },
-      });
+            const response = await fetch("http://localhost:8000/api/userinfo", {
+                method: "GET",
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    "Content-Type": "application/json",
+                },
+            });
 
-      if (response.ok) {
-        const data = await response.json();
-        localStorage.setItem("username", data.username);
-        localStorage.setItem("email", data.email);
-      } else {
-        throw new Error("Failed to fetch user info.");
-      }
-    } catch (err) {
-      console.error("Error fetching user info:", err);
-      alert("Something went wrong while fetching user details. Please try again.");
-    }
-  };
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    setError("");
-
-    const endpoint = isLogin
-      ? "http://localhost:8000/api/login"
-      : "http://localhost:8000/api/register";
-
-    const body = isLogin
-      ? { username: formData.username, password: formData.password }
-      : { username: formData.username, email: formData.email, password: formData.password };
-
-    try {
-      const response = await fetch(endpoint, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(body),
-      });
-
-      if (response.ok) {
-        const data = await response.json();
-
-        if (isLogin) {
-          sessionStorage.setItem("accessToken", data.access);
-          sessionStorage.setItem("refreshToken", data.refresh);
-          await fetchUserInfo(); 
-          setIsAuthenticated(true);
-          navigate("/"); 
-        } else {
-          alert("Registered successfully! You can now log in.");
-          setIsLogin(true);
+            if (response.ok) {
+                const data = await response.json();
+                localStorage.setItem("username", data.username);
+                localStorage.setItem("email", data.email);
+            } else {
+                throw new Error("Failed to fetch user info.");
+            }
+        } catch (err) {
+            console.error("Error fetching user info:", err);
+            alert("Something went wrong while fetching user details. Please try again.");
         }
-      } else {
-        const errorData = await response.json();
-        setError(errorData.detail || "An error occurred. Please check your input.");
-      }
-    } catch (err) {
-      setError("Something went wrong. Please try again.");
-    }
-  };
+    };
 
-  return (
-    <div className="auth-container">
-      <div className="auth-box">
-        <div className="auth-tabs">
-          <button
-            className={`auth-tab ${isLogin ? "active" : ""}`}
-            onClick={() => setIsLogin(true)}
-          >
-            Zaloguj się
-          </button>
-          <button
-            className={`auth-tab ${!isLogin ? "active" : ""}`}
-            onClick={() => setIsLogin(false)}
-          >
-            Załóż konto
-          </button>
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        setError("");
+
+        if (!isLogin && formData.password.length < 8) {
+            setError("Hasło musi mieć co najmniej 8 znaków.");
+            return;
+        }
+        const endpoint = isLogin
+            ? "http://localhost:8000/api/login"
+            : "http://localhost:8000/api/register";
+
+        const body = isLogin
+            ? {username: formData.username, password: formData.password}
+            : {username: formData.username, email: formData.email, password: formData.password};
+
+        try {
+            const response = await fetch(endpoint, {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(body),
+            });
+            if (response.ok) {
+                const data = await response.json();
+                if (isLogin) {
+                    sessionStorage.setItem("accessToken", data.access);
+                    sessionStorage.setItem("refreshToken", data.refresh);
+                    await fetchUserInfo();
+                    setIsAuthenticated(true);
+                    navigate("/");
+                } else {
+                    alert("Registered successfully! You can now log in.");
+                    setIsLogin(true);
+                }
+            } else {
+                const responseBody = await response.text();
+                let errorData;
+                try {
+                    errorData = JSON.parse(responseBody);
+                } catch (err) {
+                    console.error("Error parsing JSON:", err);
+                }
+                console.log("Error data:", errorData);
+
+                if (errorData && errorData.username) {
+                    if (errorData.username.includes("A user with that username already exists.")) {
+                        setError("A user with that username already exists.");
+                        return;
+                    }
+                }
+                if (errorData && errorData.email) {
+                    if (errorData.email.includes("user with this email already exists.")) {
+                        setError("A user with this email already exists.");
+                        return;
+                    } else if (errorData.email.includes("Enter a valid email address.")) {
+                        setError("Enter a valid email address  example@example.com");
+                        return;
+                    }
+                }
+                if (errorData && errorData.detail && typeof errorData.detail === "string" && errorData.detail.includes("No active account found with the given credentials")) {
+                    setError("User does not exist or incorrect password.");
+                    return;
+                }
+                setError("An error occurred. Please check your input.");
+                return;
+            }
+        } catch (err) {
+            console.error("Error during fetch:", err); // Logowanie błędu
+            setError("Something went wrong. Please try again.");
+        }
+    };
+
+
+    return (
+        <div className="auth-container">
+            <div className="auth-box">
+                <div className="auth-tabs">
+                    <button
+                        className={`auth-tab ${isLogin ? "active" : ""}`}
+                        onClick={() => setIsLogin(true)}
+                    >
+                        Zaloguj się
+                    </button>
+                    <button
+                        className={`auth-tab ${!isLogin ? "active" : ""}`}
+                        onClick={() => setIsLogin(false)}
+                    >
+                        Załóż konto
+                    </button>
+                </div>
+                <form onSubmit={handleSubmit} className="auth-form">
+                    <input
+                        type="text"
+                        name="username"
+                        placeholder="Username"
+                        value={formData.username}
+                        onChange={handleChange}
+                        className="auth-input"
+                    />
+                    {!isLogin && (
+                        <input
+                            type="email"
+                            name="email"
+                            placeholder="E-mail"
+                            value={formData.email}
+                            onChange={handleChange}
+                            className="auth-input"
+                        />
+                    )}
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="Hasło"
+                        value={formData.password}
+                        onChange={handleChange}
+                        className="auth-input"
+                    />
+                    {error && <p className="auth-error">{error}</p>}
+                    <button type="submit" className="auth-button">
+                        {isLogin ? "Zaloguj się" : "Zarejestruj się"}
+                    </button>
+                </form>
+            </div>
         </div>
-        <form onSubmit={handleSubmit} className="auth-form">
-          <input
-            type="text"
-            name="username"
-            placeholder="Username"
-            value={formData.username}
-            onChange={handleChange}
-            className="auth-input"
-          />
-          {!isLogin && (
-            <input
-              type="email"
-              name="email"
-              placeholder="E-mail"
-              value={formData.email}
-              onChange={handleChange}
-              className="auth-input"
-            />
-          )}
-          <input
-            type="password"
-            name="password"
-            placeholder="Hasło"
-            value={formData.password}
-            onChange={handleChange}
-            className="auth-input"
-          />
-          {error && <p className="auth-error">{error}</p>}
-          <button type="submit" className="auth-button">
-            {isLogin ? "Zaloguj się" : "Zarejestruj się"}
-          </button>
-        </form>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default LoginRegister;
